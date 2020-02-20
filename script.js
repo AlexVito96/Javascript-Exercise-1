451e9bf2c6d29864a0933950c75e9e26a51f0361
AlexVito96 <59684198+AlexVito96@users.noreply.github.com>
2020-02-20 00:19:20 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+alert("Hello Professor");
+
+let num=[10,20,33,45,1,47,13];
+
+var num1= 10;
+var num2= 20;
+var num3= 33;
+var num4= 45;
+var num5= 1;
+var num6= 47;
+var num7= 13;
+
+alert(num1);
+alert(num2);
+alert(num3);
+alert(num4);
+alert(num5);
+alert(num6);
+alert(num7);
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
+console.log(num6);
+console.log(num7);
+
+var avg= ((num1 + num2 + num3 + num4 + num5 + num6 + num7)/num.length);
+
+alert("The average of all seven numbers is: " + avg);
+
+
+console.log("The average of all seven numbers is: " + avg);
